8719dbebc8753c09354c064b51b73859eb087680
john rummell <john.rummell@gmail.com@581d0318-ef2c-11de-aed4-3956c0e5a634>
2009-12-24 14:21:43 +0000

    added License file header

--- SteamAchievements.Services/IAchievementService.cs
+++ SteamAchievements.Services/IAchievementService.cs
@@ -1,3 +1,24 @@
+#region License
+
+// Copyright 2010 John Rummell
+// 
+// This file is part of SteamAchievements.
+// 
+//     SteamAchievements is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     SteamAchievements is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with SteamAchievements.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
 using System.Collections.ObjectModel;
 using System.ServiceModel;
 using System.ServiceModel.Web;
@@ -14,7 +35,8 @@ namespace SteamAchievements.Services
         AchievementCollection GetAchievements(string steamUserId, int gameId);
 
         [OperationContract]
-        [WebInvoke(Method = "POST", UriTemplate = "/GetGames", RequestFormat= WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        [WebInvoke(Method = "POST", UriTemplate = "/GetGames", RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
         Collection<Game> GetGames(); // returning IEnumerable<GameDTO> causes a serialization exception
 
         [OperationContract]
--- SteamAchievements.Services/SteamCommunityManager.cs
+++ SteamAchievements.Services/SteamCommunityManager.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿#region License
+
+// Copyright 2010 John Rummell
+// 
+// This file is part of SteamAchievements.
+// 
+//     SteamAchievements is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     SteamAchievements is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with SteamAchievements.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
 using System.Net;
 using System.Text.RegularExpressions;
 using SteamAchievements.Data;
@@ -16,7 +37,8 @@ namespace SteamAchievements.Services
 
             foreach (Game game in _service.GetGames())
             {
-                string statsUrl = String.Format("http://steamcommunity.com/id/{0}/stats/{1}?tab=achievements", steamUserId,
+                string statsUrl = String.Format("http://steamcommunity.com/id/{0}/stats/{1}?tab=achievements",
+                                                steamUserId,
                                                 game);
                 string html = GetStatsHtml(statsUrl);
 
