377cb611dd838e9fc8cfef7f677cd48e0f421e23
John Rummell <john@jrummell.com>
2009-12-24 14:21:43 +0000

    added License file header

--- SteamAchievements/Default.aspx.cs
+++ SteamAchievements/Default.aspx.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿#region License
+
+// Copyright 2010 John Rummell
+// 
+// This file is part of SteamAchievements.
+// 
+//     SteamAchievements is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     SteamAchievements is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with SteamAchievements.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
 using System.Web.UI;
 using SteamAchievements.Data;
 
@@ -6,6 +27,8 @@ namespace SteamAchievements
 {
     public partial class Default : Page
     {
+        protected string SteamUserId { get; private set; }
+
         protected override void OnInit(EventArgs e)
         {
             base.OnInit(e);
@@ -33,11 +56,6 @@ namespace SteamAchievements
             {
                 Response.Write(ex.Message);
             }
-        }
-
-        protected string SteamUserId
-        {
-            get; private set;
         }
     }
 }
--- SteamAchievements/Properties/AssemblyInfo.cs
+++ SteamAchievements/Properties/AssemblyInfo.cs
@@ -1,10 +1,31 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region License
+
+// Copyright 2010 John Rummell
+// 
+// This file is part of SteamAchievements.
+// 
+//     SteamAchievements is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     SteamAchievements is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with SteamAchievements.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("FBMLCanvasSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +38,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
 
 // Version information for an assembly consists of the following four values:
@@ -31,5 +54,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
