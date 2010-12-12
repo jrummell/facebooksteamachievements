9d4091669273e1acaad006ed5969aebcd5bfb14b
John Rummell <john@jrummell.com>
2010-12-12 03:00:54 +0000

    UserAchievement - replaced SteamUserId with FacebookUserId

--- Database/Tables/dbo.steam_UserAchievement.sql
+++ Database/Tables/dbo.steam_UserAchievement.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[steam_UserAchievement]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[SteamUserId] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[FacebookUserId] [bigint] NOT NULL CONSTRAINT [DF_steam_UserAchievement_FacebookUserId] DEFAULT ((0)),
 [AchievementId] [int] NOT NULL,
 [Date] [datetime] NOT NULL,
 [Published] [bit] NOT NULL
@@ -9,9 +9,7 @@ CREATE TABLE [dbo].[steam_UserAchievement]
 GO
 ALTER TABLE [dbo].[steam_UserAchievement] ADD CONSTRAINT [PK_steam_UserAchievement] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
 GO
-CREATE UNIQUE NONCLUSTERED INDEX [IX_steam_UserAchievement] ON [dbo].[steam_UserAchievement] ([SteamUserId], [AchievementId]) ON [PRIMARY]
+ALTER TABLE [dbo].[steam_UserAchievement] ADD CONSTRAINT [FK_steam_UserAchievement_steam_Achievement] FOREIGN KEY ([AchievementId]) REFERENCES [dbo].[steam_Achievement] ([Id])
 GO
-ALTER TABLE [dbo].[steam_UserAchievement] ADD CONSTRAINT [FK_steam_UserAchievement_steam_Achievement1] FOREIGN KEY ([AchievementId]) REFERENCES [dbo].[steam_Achievement] ([Id])
-GO
-ALTER TABLE [dbo].[steam_UserAchievement] ADD CONSTRAINT [FK_steam_UserAchievement_steam_User] FOREIGN KEY ([SteamUserId]) REFERENCES [dbo].[steam_User] ([SteamUserId])
+ALTER TABLE [dbo].[steam_UserAchievement] ADD CONSTRAINT [FK_steam_UserAchievement_steam_Achievement1] FOREIGN KEY ([FacebookUserId]) REFERENCES [dbo].[steam_User] ([FacebookUserId])
 GO
