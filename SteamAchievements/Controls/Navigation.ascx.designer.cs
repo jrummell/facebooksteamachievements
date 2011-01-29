86380fc0c80ca318407ea6644d6f1138305f94d7
john rummell <john.rummell@gmail.com@581d0318-ef2c-11de-aed4-3956c0e5a634>
2011-01-29 04:12:12 +0000

    SteamAchievements/Controls/Navigation.ascx - fixed help link.

--- SteamAchievements/Controls/Navigation.ascx.designer.cs
+++ SteamAchievements/Controls/Navigation.ascx.designer.cs
@@ -38,5 +38,14 @@ namespace SteamAchievements.Controls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::SteamAchievements.Controls.CanvasLink menuSettingsLink;
+        
+        /// <summary>
+        /// helpLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink helpLink;
     }
 }
