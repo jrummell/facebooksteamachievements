a188871dd9925a204ee642cc960e2b4dd0170046
John Rummell <john@jrummell.com>
2011-01-30 08:06:02 +0000

    SteamAchievements/Publish.aspx - added message about scrolling down to find publish dialog. Added a second column for unpublished achievements to avoid some scrolling issues.

--- SteamAchievements/js/publish.js
+++ SteamAchievements/js/publish.js
@@ -54,20 +54,35 @@ function displayAchievements(achievements)
 {
     _newAchievements = achievements;
 
-    $("#achievementTemplate").tmpl(achievements).appendTo("#newAchievements");
+    // pair the achievements so that the templates plugin can display them in a 2 column table
+    var pairedAchievements = new Array();
+    for (var i = 0; i < achievements.length; i=i+2)
+    {
+        var pair = { achievement1: achievements[i], achievement2: achievements[i+1] };
+        pairedAchievements.push(pair);
+    }
+
+    $("#achievementTemplate").tmpl(pairedAchievements).appendTo("#newAchievements");
 
     // allow user to select only 5 achievements since only 5 images can be displayed at a time
     $("#newAchievements .achievement :checkbox, #newAchievements .achievement img").click(function ()
     {
-        $(this).parents(".achievement").toggleClass("selected");
-        if (this.tagName == "IMG")
-        {
-            var checkbox = $(this).prev()[0];
-            checkbox.checked = !checkbox.checked;
-        }
-
         var $checked = $("#newAchievements :checked");
         var disableUnchecked = $checked.length >= 5;
+
+        // toggle the selection if there are less than 5 selected or if the current item is already selected
+        var $achievementDiv = $(this).parents(".achievement");
+        if (!disableUnchecked || $achievementDiv.hasClass("selected"))
+        {
+            $achievementDiv.toggleClass("selected");
+            if (this.tagName == "IMG")
+            {
+                var checkbox = $(this).prev()[0];
+                checkbox.checked = !checkbox.checked;
+            }
+        }
+
+        // disable unchecked boxes if there are 5 checked
         $("#newAchievements .achievement :checkbox").filter(function (index)
         {
             return !this.checked;
