5fcf1e07b4d1b33398063e48799d077e3a284e4a
john rummell <john.rummell@gmail.com@581d0318-ef2c-11de-aed4-3956c0e5a634>
2011-02-04 02:44:31 +0000

    steam_UserAchievement - added Hidden

--- Database/Tables/dbo.steam_UserAchievement.sql
+++ Database/Tables/dbo.steam_UserAchievement.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[steam_UserAchievement]
 [FacebookUserId] [bigint] NOT NULL CONSTRAINT [DF_steam_UserAchievement_FacebookUserId] DEFAULT ((0)),
 [AchievementId] [int] NOT NULL,
 [Date] [datetime] NOT NULL,
-[Published] [bit] NOT NULL
+[Published] [bit] NOT NULL,
+[Hidden] [bit] NOT NULL CONSTRAINT [DF_steam_UserAchievement_Hidden] DEFAULT ((0))
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[steam_UserAchievement] ADD CONSTRAINT [PK_steam_UserAchievement] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
