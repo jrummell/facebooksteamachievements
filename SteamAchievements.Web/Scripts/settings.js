071793f4cb6a06610a42471caa1b5e8a5e5c0385
John Rummell <john@jrummell.com>
2011-04-27 01:42:42 +0000

    (Closes issue #92) : Display error when Steam community profile isn't accessible on Settings page

---
+++ SteamAchievements.Web/Scripts/settings.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="jquery-vsdoc.js" />
+/// <reference path="jquery.message.js" />
+/// <reference path="achievements.js" />
+
+$(document).ready(function ()
+{
+    var logSelector = "#log";
+    $achievements.init("", logSelector, false);
+
+    $steamUserId = $("#SteamUserId");
+    $steamUserId.change(function ()
+    {
+        checkProfile(this.value);
+    });
+
+    $("#steamIdError").message({ type: "error", dismiss: false }).hide();
+});
+
+function checkProfile(steamUserId)
+{
+    $("#steamIdError").hide();
+    $("#steamIdVerified").hide();
+    var ondone = function (profile)
+    {
+        if (profile == null)
+        {
+            $("#steamIdError").show();
+            return;
+        }
+        else
+        {
+            $("#steamIdVerified").show();
+        }
+    };
+
+    $achievements.getProfile(ondone, null, steamUserId);
+}
