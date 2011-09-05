7929a6200f80d6e87142c520a1caaada7d801550
John Rummell <john@jrummell.com>
2011-09-05 05:58:38 +0000

    Added steam_Achievement.ApiName

--- Database/Tables/dbo.steam_Achievement.sql
+++ Database/Tables/dbo.steam_Achievement.sql
@@ -1,12 +1,13 @@
 CREATE TABLE [dbo].[steam_Achievement]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[Name] [varchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ApiName] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_steam_Achievement_ApiName] DEFAULT (N''),
+[Name] [nvarchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [GameId] [int] NOT NULL,
-[Description] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Description] [nvarchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ImageUrl] [varchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
-CREATE NONCLUSTERED INDEX [IX_steam_Achievement] ON [dbo].[steam_Achievement] ([Name], [GameId]) ON [PRIMARY]
+
 
 
 
