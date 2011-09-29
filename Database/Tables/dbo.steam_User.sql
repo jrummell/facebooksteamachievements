abc6375c6b0ad4d5b7eff4cfebe27bdc97580f2a
John Rummell <john@jrummell.com>
2011-09-29 01:09:02 +0000

    Added steam_User.Language

--- Database/Tables/dbo.steam_User.sql
+++ Database/Tables/dbo.steam_User.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[steam_User]
 [SteamUserId] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [AccessToken] [varchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_steam_User_AccessToken] DEFAULT (N''),
 [AutoUpdate] [bit] NOT NULL CONSTRAINT [DF_steam_User_AutoUpdate] DEFAULT ((0)),
-[PublishDescription] [bit] NOT NULL CONSTRAINT [DF_steam_User_PublishDescription] DEFAULT ((1))
+[PublishDescription] [bit] NOT NULL CONSTRAINT [DF_steam_User_PublishDescription] DEFAULT ((1)),
+[Language] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[steam_User] ADD CONSTRAINT [PK_steam_User] PRIMARY KEY CLUSTERED  ([FacebookUserId]) ON [PRIMARY]
