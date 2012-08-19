0a050b4dc7990c30fde7c6e05796b6d800c28fb6
john rummell <john.rummell@gmail.com@581d0318-ef2c-11de-aed4-3956c0e5a634>
2012-08-19 04:53:07 +0000

    CanvasSignedRequestAttribute - now throwing and catching InvalidSignedRequestException so that I can log useful exception info.

--- SteamAchievements.Web/Models/InvalidSignedRequestException.cs
+++ SteamAchievements.Web/Models/InvalidSignedRequestException.cs
@@ -1,16 +1,46 @@
-﻿using System;
+﻿#region License
+
+//  Copyright 2012 John Rummell
+//  
+//  This file is part of SteamAchievements.
+//  
+//      SteamAchievements is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation, either version 3 of the License, or
+//      (at your option) any later version.
+//  
+//      SteamAchievements is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//  
+//      You should have received a copy of the GNU General Public License
+//      along with SteamAchievements.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
 
 namespace SteamAchievements.Web.Models
 {
     public class InvalidSignedRequestException : Exception
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="InvalidSignedRequestException"/> class.
+        ///   Initializes a new instance of the <see cref="InvalidSignedRequestException" /> class.
         /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="innerException">The inner exception.</param>
+        /// <param name="message"> The message. </param>
+        /// <param name="innerException"> The inner exception. </param>
         public InvalidSignedRequestException(string message, Exception innerException)
             : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="InvalidSignedRequestException" /> class.
+        /// </summary>
+        /// <param name="message"> The message. </param>
+        public InvalidSignedRequestException(string message)
+            : base(message)
         {
         }
     }
