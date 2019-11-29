3cf0ad1d2c9602fbbdb3052f23165f214d9652e0
John Rummell <john@rummellweb.com>
2019-11-29 01:13:32 -0500

    added new SPA project with vue

---
+++ SteamAchievements.Web.Spa/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SteamAchievements.Web.Spa.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+    }
+}
