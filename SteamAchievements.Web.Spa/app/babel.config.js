90d04eb82e4e4a856e48f14a95a362141ed1fdbd
John Rummell <john@rummellweb.com>
2019-12-14 14:28:02 -0500

    cleanup

--- SteamAchievements.Web.Spa/app/babel.config.js
+++ SteamAchievements.Web.Spa/app/babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-    presets: [
-        "@vue/cli-plugin-babel/preset"
-        // ["@babel/env", {
-        //     modules: false,
-        //     useBuiltIns: "usage",
-        //     corejs: "3"
-        // }], ["@vue/cli-plugin-babel/preset", {
-        //     modules: false,
-        //     useBuiltIns: "usage",
-        //     corejs: "3"
-        // }]
-    ]
+    presets: ["@vue/cli-plugin-babel/preset"]
 };
